a246cd41fddde575495d36b8b1190b43aede4652
alexandre <alexandre.fouellet@gmail.com>
2024-06-29 14:39:00 -0400

    wrote the about me, added the photo

--- public/js/language-switch.js
+++ public/js/language-switch.js
@@ -2,17 +2,33 @@ let lang = 'en';
 
 const siteContent = {
     'intro-h1': {
-      'en': 'welcome test',
-      'fr': 'Bonjour tout le monde!',
+      'en': "HEY, I'M ALEX OUELLET FOURNIER",
+      'fr': "BONJOUR, JE SUIS ALEX OUELLET FOURNIER",
     },
 	'intro-p': {
-		'en': 'english version',
-		'fr': 'en francais!',
+		'en': 'A junior full-stack developer driven by curiosity and a passion for learning. I enjoy diving into both frontend and backend technologies, eager to expand my skills and contribute to innovative projects',
+		'fr': "Un développeur full-stack junior motivé par la curiosité et la passion d'apprendre. J'aime me plonger dans les technologies frontend et backend, désireux d'élargir mes compétences et de contribuer à des projets innovants.",
 	},
 	'language': {
 		'en': 'fr',
 		'fr': 'en',
-	}
+	},
+	'about': {
+		'en': 'About',
+		'fr': 'À propos',
+	},
+	'project': {
+		'en': 'Projects',
+		'fr': 'Projets',
+	},
+	'home': {
+		'en': 'Home',
+		'fr': 'Accueil',
+	},
+	'project-button': {
+		'en': 'PROJECTS',
+		'fr': 'PROJETS',
+	},
   };
   
 function switchLanguage() {
@@ -24,7 +40,8 @@ function switchLanguage() {
 	}
 
 	document.documentElement.lang = lang; 
-
+	typeEffect(siteContent['intro-h1'][lang], speed); 
+	
 	document.querySelectorAll('[id]').forEach((element) => {
 		const id = element.id;
 		if (siteContent[id]) 
